b51ece55ff725a8c075e219301e593bfb161f3f7
Mythical-Github <MythicalData@gmail.com>
2025-02-13 19:48:11 -0500

    updated to pywinctl

--- src/unreal_auto_mod/window_management.py
+++ src/unreal_auto_mod/window_management.py
@@ -1,27 +1,28 @@
+import ctypes
 
-import pygetwindow
+import pywinctl
 import screeninfo
 
-from unreal_auto_mod import log as log
+from unreal_auto_mod import log
 
 
 def does_window_exist(window_title: str, use_substring_check: bool = False) -> bool:
     try:
         if use_substring_check:
-            all_window_titles = pygetwindow.getAllTitles()
+            all_window_titles = pywinctl.getAllTitles()
             matched_windows = [window for window in all_window_titles if window_title in window]
         else:
-            all_window_titles = pygetwindow.getAllTitles()
+            all_window_titles = pywinctl.getAllTitles()
             matched_windows = [window for window in all_window_titles if window_title == window]
         return len(matched_windows) > 0
-    except pygetwindow.PyGetWindowException as e:
+    except RuntimeError as e:
         log.logging(f'Error: An error occurred: {e}')
         return False
 
 
 def get_windows_by_title(window_title: str, use_substring_check: bool = False) -> list:
     matched_windows = []
-    all_windows = pygetwindow.getAllWindows()
+    all_windows = pywinctl.getAllWindows()
     if use_substring_check:
         try:
             matched_windows = [window for window in all_windows if window_title in window.title]
@@ -37,7 +38,7 @@ def get_windows_by_title(window_title: str, use_substring_check: bool = False) -
     return matched_windows
 
 
-def get_window_by_title(window_title: str, use_substring_check: bool = False) -> pygetwindow.Win32Window:
+def get_window_by_title(window_title: str, use_substring_check: bool = False) -> pywinctl.Window:
     windows = get_windows_by_title(window_title, use_substring_check)
     if not windows:
         log.log_message(f'Warning: No windows found with title "{window_title}"')
@@ -45,19 +46,19 @@ def get_window_by_title(window_title: str, use_substring_check: bool = False) ->
     return windows[0]
 
 
-def minimize_window(window: pygetwindow.Win32Window):
-    pygetwindow.Window.minimize(window)
+def minimize_window(window: pywinctl.Window):
+    pywinctl.Window.minimize(window)
 
 
-def maximize_window(window: pygetwindow.Win32Window):
-    pygetwindow.Window.maximize(window)
+def maximize_window(window: pywinctl.Window):
+    pywinctl.Window.maximize(window)
 
 
-def close_window(window: pygetwindow.Win32Window):
-    pygetwindow.Window.close(window)
+def close_window(window: pywinctl.Window):
+    pywinctl.Window.close(window)
 
 
-def move_window_to_monitor(window: pygetwindow.Win32Window, monitor_index: int = 0):
+def move_window_to_monitor(window: pywinctl.Window, monitor_index: int = 0):
     screen_info = screeninfo.get_monitors()
     if monitor_index < len(screen_info):
         monitor = screen_info[monitor_index]
@@ -66,16 +67,15 @@ def move_window_to_monitor(window: pygetwindow.Win32Window, monitor_index: int =
         log.log_message('Monitor: Invalid monitor index.')
 
 
-def set_window_size(window: pygetwindow.Win32Window, width: int, height: int):
+def set_window_size(window: pywinctl.Window, width: int, height: int):
     window.size = (width, height)
 
 
 def change_window_name(window_name: str):
-    import ctypes
     ctypes.windll.kernel32.SetConsoleTitleW(window_name)
 
 
-def move_window(window: pygetwindow.Win32Window, window_settings: list):
+def move_window(window: pywinctl.Window, window_settings: list):
     monitor_index = window_settings['monitor']
     if monitor_index is not None:
         move_window_to_monitor(window, monitor_index)
