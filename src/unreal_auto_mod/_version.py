6a649cd182e7d21c4e967be1e7ab0241000de839
Mythical-Github <MythicalData@gmail.com>
2025-03-26 22:16:18 -0400

    documentation setup testing

--- src/unreal_auto_mod/_version.py
+++ src/unreal_auto_mod/_version.py
@@ -5,7 +5,8 @@ __all__ = ["__version__", "__version_tuple__", "version", "version_tuple"]
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple, Union
+    from typing import Tuple
+    from typing import Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
 else:
@@ -16,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '2.0.0a2.dev17+g9387b84.d20250311'
-__version_tuple__ = version_tuple = (2, 0, 0, 'dev17', 'g9387b84.d20250311')
+__version__ = version = '2.0.0a2.dev48+gffe0c3e.d20250327'
+__version_tuple__ = version_tuple = (2, 0, 0, 'dev48', 'gffe0c3e.d20250327')
