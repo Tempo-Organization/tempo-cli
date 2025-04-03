100fa663162de8e9bfae9fe034d31f9eeaf18d48
Mythical-Github <MythicalData@gmail.com>
2025-04-03 17:35:35 -0400

    Testing upload on testing repo

--- src/unreal_auto_mod/_version.py
+++ src/unreal_auto_mod/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '2.0.0a2.dev104+ga76decd.d20250403'
-__version_tuple__ = version_tuple = (2, 0, 0, 'dev104', 'ga76decd.d20250403')
+__version__ = version = '2.0.0a2.dev105+g4a75e68'
+__version_tuple__ = version_tuple = (2, 0, 0, 'dev105', 'g4a75e68')
