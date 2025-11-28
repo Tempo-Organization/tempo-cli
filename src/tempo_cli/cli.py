0a2a2dfe7de701df75796778c94afb8925a903c8
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2025-11-28 07:17:52 -0500

    fix(cli.py): fixed issue with paths
    fixed issue where pathlib.Path was being used instead of an str

--- src/tempo_cli/cli.py
+++ src/tempo_cli/cli.py
@@ -26,7 +26,7 @@ default_logs_dir = os.path.normpath(f"{file_io.SCRIPT_DIR}/logs")
 default_output_releases_dir = os.path.normpath(os.path.join(file_io.SCRIPT_DIR, "dist"))
 default_releases_dir = os.path.normpath(
     os.path.join(
-        settings.settings_information.settings_json_dir, "mod_packaging", "releases"
+        str(settings.settings_information.settings_json_dir), "mod_packaging", "releases"
     )
 )
 
