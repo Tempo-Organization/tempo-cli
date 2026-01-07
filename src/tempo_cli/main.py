1104a486ccd730f77205b74e333719b010931587
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2026-01-07 02:53:53 -0500

    fix(main.py): fixed colorization of prompts
    switched to a monkey patched prompt function to enable rich usage

--- src/tempo_cli/main.py
+++ src/tempo_cli/main.py
@@ -1,20 +1,26 @@
-
 import click
 from click.termui import _build_prompt as _original_build_prompt
-
 from tempo_cli import cli
+from rich.style import Style
+from rich.text import Text
+
+from tempo_core import log_info
+from tempo_core.console import console
 
 
-def _style_prompt(prompt: str) -> str:
-    return click.style(prompt, fg="blue", bg="white", bold=False, dim=True)
+def _rich_build_prompt(prompt_text: str, fg=log_info.LOG_INFO['default_color'], bg=log_info.LOG_INFO['background_color']):
+    style = Style(color=f"rgb{fg}", bgcolor=f"rgb{bg}")
+    rich_text = Text(prompt_text, style=style)
+    console.print(rich_text, end="")
+    return ""
 
-def styled_build_prompt(
-    text, prompt_suffix, show_default, default, show_choices, type_
-):
+
+def styled_build_prompt(text, prompt_suffix, show_default, default, show_choices, type_):
     prompt = _original_build_prompt(
         text, prompt_suffix, show_default, default, show_choices, type_
     )
-    return _style_prompt(prompt)
+    _rich_build_prompt(prompt)
+    return ""
 
 
 click.termui._build_prompt = styled_build_prompt
