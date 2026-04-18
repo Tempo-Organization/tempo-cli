c3140189810c4feeb0f4327ffe43f1a366f03363
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2026-04-17 23:00:20 -0400

    feat(All): changed how cache works
    updated cache to be better, and made sep modules

--- src/tempo_cli/commands/tool.py
+++ src/tempo_cli/commands/tool.py
@@ -51,10 +51,5 @@ for name, help_text, func in tools:
         type=bool,
         help="Should the installed program be run after installation.",
     )
-    @click.argument(
-        "output_directory",
-        type=click.Path(exists=False, resolve_path=True, path_type=pathlib.Path),
-        default=os.path.normpath(f'{os.getcwd()}/tools/{name}')
-    )
-    def _command(output_directory, run_after_install, func=func):
-        func(output_directory=output_directory, run_after_install=run_after_install)
+    def _command(run_after_install, func=func):
+        func(run_after_install=run_after_install)
